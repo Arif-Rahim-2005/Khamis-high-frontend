f34b771e77bd58b85e61391ed281be812b3b28b5
arifrahim <arifrahimjamal@gmail.com>
2025-10-31 15:09:38 +0300

    fixed page reload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   safelist: [
+//     {
+//       pattern:
+//         /(bg|text|border)-(green|amber|blue|gray)-(50|100|200|300|400|500|600|700)/,
+//     },
+//   ],
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
   safelist: [
     {
       pattern:
